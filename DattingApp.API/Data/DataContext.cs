2dcabfe9c6283c28e4c3b4ab87a758d4ec429a6d
Pablo Rivas Sánchez <pablo.rivas@avanade.com>
2018-11-18 10:20:07 +0100

    first commit

---
+++ DattingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DattingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
