83d9f059b6d776bc1d22b3bf8577365a4312f7cf
Pablo Rivas Sánchez <programacionagil@gmail.com>
2019-04-26 11:48:26 +0200

    fix - gitignore problems

--- DattingApp.API/Data/DataContext.cs
+++ DattingApp.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace DattingApp.API.Data
     {
         public DataContext(DbContextOptions options) : base(options) { }
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
